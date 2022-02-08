569e8887e438f4d37f7c6badc9915e1203c45510
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-02-08 13:38:25 -0600

    Finished the Highest and Average stock price graphs

--- public/index.js
+++ public/index.js
@@ -1,13 +1,15 @@
 async function main() {
     // tweleveData API key 1f32e5a721c54c20b757a6963e0052d8
     // 4 stocks we need to get GME, MSFT, DIS, and BNTX
-    const url =
-        "https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&format=JSON&apikey=1f32e5a721c54c20b757a6963e0052d8";
+    let symbols = "GME,MSFT,DIS,BNTX";
+    let interval = "1day";
+    const apikey = "1f32e5a721c54c20b757a6963e0052d8";
+    const url = `https://api.twelvedata.com/time_series?symbol=${symbols}&interval=${interval}&format=JSON&apikey=${apikey}`;
     let response = await fetch(url);
     const { GME, MSFT, DIS, BNTX } = await response.json();
     // const { GME, MSFT, DIS, BNTX } = mockData;
     const stocks = [GME, MSFT, DIS, BNTX];
-    console.log(stocks);
+    // console.log(stocks);
 
     const timeChartCanvas = document.querySelector("#time-chart");
     const highestPriceChartCanvas = document.querySelector(
@@ -17,55 +19,41 @@ async function main() {
         "#average-price-chart"
     );
 
-    //Test code
-    const labels = ["January", "February", "March", "April", "May", "June"];
-    const graphColors = [
-        "red",
-        "blue",
-        "purple",
-        "aqua",
-        "green",
-        "brown",
-        "black",
-        "yellow",
-    ];
     function getColor(stock) {
         if (stock === "GME") {
-            return graphColors[0];
             return "rgba(61, 161, 61, 0.7)";
         }
         if (stock === "MSFT") {
-            return graphColors[1];
             return "rgba(209, 4, 25, 0.7)";
         }
         if (stock === "DIS") {
-            return graphColors[2];
             return "rgba(18, 4, 209, 0.7)";
         }
         if (stock === "BNTX") {
-            return graphColors[3];
             return "rgba(166, 43, 158, 0.7)";
         }
     }
 
-    // const data = {
-    //     labels: labels,
-    //     datasets: [
-    //         {
-    //             label: "2021-2-5", //"My First dataset",
-    //             backgroundColor: "rgb(255, 99, 132)",
-    //             borderColor: "rgb(255, 99, 132)",
-    //             data: [0, 10, 5, 2, 20, 30, 45],
-    //         },
-    //     ],
-    // };
+    function getHighestValue(stockArr) {
+        // console.log("stock " + stockArr);
+        // let highVal = 0;
+        // stockArr.forEach((stock) => {
+        //     if (parseFloat(stock) > highVal) {
+        //         highVal = parseFloat(stock);
+        //     }
+        // });
+        // // console.log(highVal);
+        // return "" + highVal;
+        return Math.max(...stockArr);
+    }
 
-    // const config = {
-    //     type: "line",
-    //     data: data,
-    //     options: {},
-    // };
-    // const myChart = new Chart(timeChartCanvas, config);
+    function getAverageValue(stockArr) {
+        // let average = stockArr.reduce((a, b) => a + b, 0) / stockArr.length;
+        let total = 0;
+        stockArr.map((stock) => (total += parseFloat(stock)));
+        // console.log(total / stockArr.length, total, stockArr.length);
+        return total / stockArr.length;
+    }
 
     new Chart(timeChartCanvas.getContext("2d"), {
         type: "line",
@@ -77,6 +65,47 @@ async function main() {
                 backgroundColor: getColor(stock.meta.symbol),
                 borderColor: getColor(stock.meta.symbol),
             })),
+        },
+    });
+
+    new Chart(highestPriceChartCanvas.getContext("2d"), {
+        type: "bar",
+        data: {
+            labels: stocks.map((stock) => stock.meta.symbol),
+            datasets: [
+                {
+                    label: "Highest Price",
+                    data: stocks.map((stock) =>
+                        getHighestValue(stock.values.map((value) => value.high))
+                    ),
+                    backgroundColor: stocks.map((stock) =>
+                        getColor(stock.meta.symbol)
+                    ),
+                    borderColor: stocks.map((stock) =>
+                        getColor(stock.meta.symbol)
+                    ),
+                },
+            ],
+        },
+    });
+    new Chart(averagePriceChartCanvas.getContext("2d"), {
+        type: "doughnut",
+        data: {
+            labels: stocks.map((stock) => stock.meta.symbol),
+            datasets: [
+                {
+                    label: "Price",
+                    data: stocks.map((stock) =>
+                        getAverageValue(stock.values.map((value) => value.high))
+                    ),
+                    backgroundColor: stocks.map((stock) =>
+                        getColor(stock.meta.symbol)
+                    ),
+                    borderColor: stocks.map((stock) =>
+                        getColor(stock.meta.symbol)
+                    ),
+                },
+            ],
         },
     });
 }
